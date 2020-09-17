1a41296a70a46d8d119b00a57ae0bdf9d2ed9b28
Aleksander B. Mosberg <aleksander.b.mosberg@gmail.com>
2020-09-17 13:26:20 +0200

    Fix remaining 'box-forced' in fieldArray

--- scriptFields.py
+++ scriptFields.py
@@ -1,6 +1,5 @@
 from fieldArray import FieldArray
 from metaReader import get_pixel_size
-from lattice import loadLattice
 
 path = 'data/images/main/png_steinar'
 prep_path = 'data/images/main/prep'
@@ -10,6 +9,7 @@ scale = get_pixel_size('data/images/main/originals/Topdown_immersion_5kV86pA_Arr
 # So new scale should be 4x original scale.
 
 # fields = FieldArray(8, 8, 15, 18, path, 4.8, '.png')
+# fields = FieldArray(8, 8, 15, 18, path, scale*4, '.png')
 fields = FieldArray(8, 8, 15, 18, prep_path, scale*4, '.png')
 # fields = fields.prepFields(kernel_size=3, prep_path=prep_path)
 # fld = fields.fields[0]
@@ -33,9 +33,25 @@ fields.ensureBlobsByPoint()
 
 # fields.plotLatticesWithBlobs()
 # fields.plotYield(5)
-# fields.plotYield(2)
+# fields.plotYield(1)
 # fields.plotMeanDiameters({'title': 'Mean droplet diameter'})
+fields.plotMeanDiameters()
 # fields.plotDiameterHistograms()
 # fields.plotFancyDiameterHistogram()
-fields.plotDisplacementScatterPlots()
-# fields.plotSingleScatterPlot(6)
+# fields.plotDisplacementScatterPlots()
+# fields.plotSingleScatterPlot(55)
+
+# fields.fields[54].plotLikeTiles()
+# fields.plotWithConnects(6)
+# fields.plotWithConnects(56)
+# fields.plotAllYields()
+
+# doses = ds.doses_sorted()
+# pos = ds.dose_argsort()
+
+# diam = fields.getMedianDiameters()
+# diam_s = [diam[i] for i in pos]
+
+# plt.plot(doses,diam_s)
+
+blobs = [fld.getBlobCount() for fld in fields.fields]
